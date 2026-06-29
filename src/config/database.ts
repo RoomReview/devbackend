a292a1dc9dd822c4ce580ea6f85b10721e225cde
Alamkheer Husainul Fareedh M <30697625+alamkheer@users.noreply.github.com>
2026-06-29 22:50:04 +0100

    fix: reset migrations; area schema dummy commit

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,10 +1,14 @@
 import 'dotenv/config';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../generated/prisma/client.js';
+import logger from '@utils/logger.js';
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter, log: ['query', 'error'] });
 
+prisma.$on('query', (e) => {
+  logger.info({ service: 'Database', function: 'query' }, 'Duration: ' + e.duration + 'ms');
+});
 export default prisma;
