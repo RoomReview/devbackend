205cdea2219b654c659da34857469b7e36eefdb0
YelyseiShcherbak <yelysei@roomreview.co.uk>
2026-08-19 01:12:06 +0200

    Add manage permissions for locations and reports

--- src/config/permissions.ts
+++ src/config/permissions.ts
@@ -1,8 +1,9 @@
-// TBD: future permissions table implementation
 
 import { UserRole } from '@/generated/prisma/enums';
 
 export const permissions = {
   'view:users:all': [UserRole.ADMIN],
   'view:users:self': [UserRole.ADMIN, UserRole.LANDLORD, UserRole.TENANT],
+  'manage:locations': [UserRole.ADMIN],
+  'manage:reports': [UserRole.ADMIN],
 } as const;
