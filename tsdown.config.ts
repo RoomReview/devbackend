482f8f2e22827cd5d02dc6c5fa4b4a7af6ea38dd
YelyseiShcherbak <yelysei@roomreview.co.uk>
2026-09-21 18:43:03 +0200

    Modify tsdown configuration for entry and aliases
    Updated entry point and added path aliases in the configuration.

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,13 +1,29 @@
 import { defineConfig } from 'tsdown';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const rootDirectory = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  // entry: ["src/**/*.ts"],
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
   clean: true,
-  target: "node22", // Compatible with most Node versions
+  target: "node22",
   treeshake: true,
   minify: process.env?.NODE_ENV === "production",
-  sourcemap: !(process.env?.NODE_ENV === "production"),
+  sourcemap: process.env?.NODE_ENV !== "production",
   outDir: "dist",
+  
+  alias: {
+    '@': path.resolve(rootDirectory, './src'),
+    '@config': path.resolve(rootDirectory, './src/config'),
+    '@controllers': path.resolve(rootDirectory, './src/controllers'),
+    '@middleware': path.resolve(rootDirectory, './src/middleware'),
+    '@models': path.resolve(rootDirectory, './src/models'),
+    '@routes': path.resolve(rootDirectory, './src/routes'),
+    '@services': path.resolve(rootDirectory, './src/services'),
+    '@utils': path.resolve(rootDirectory, './src/utils'),
+    '@types': path.resolve(rootDirectory, './src/types'),
+    '@dto': path.resolve(rootDirectory, './src/dto'),
+  },
 });
